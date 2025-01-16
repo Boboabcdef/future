28590a7bc0d0469ae1bcee45cfc6f340b8bd64b3
bo.xiao <bo.xiao@advantech.com>
2025-01-16 14:31:28 +0800

    add render web run baidu AI model

--- test_api.py
+++ test_api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -41,4 +42,5 @@ def chat():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
